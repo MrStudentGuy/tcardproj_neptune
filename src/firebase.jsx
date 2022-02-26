914610a6a38c2ff0ea1f99ab8f3a6c53d9d9420d
MrStudentGuy <Aryan.1301021@dais.edu.in>
2022-02-27 01:11:24 +0530

    2/27/2022 1:11AM IST New commit with addes google integration.

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,22 +1,35 @@
-import dotenv from 'dotenv'
-dotenv.config()
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID 
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID 
+    apiKey: "AIzaSyCaAOIObBvR4ZidtYmr4ccmJvnbAeIHamY",
+    authDomain: "neptune-29d31.firebaseapp.com",
+    projectId: "neptune-29d31",
+    storageBucket: "neptune-29d31.appspot.com",
+    messagingSenderId: "239829980161",
+    appId: "1:239829980161:web:397eb51687b53649071c00"
 };
  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+export const provider = new GoogleAuthProvider();
+
+
+export const auth = getAuth();
+
+
+
--- src/screens/acntloginsignup.jsx
+++ src/screens/acntloginsignup.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import './loginsignup.css';
-import { getAuth, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
-
-const auth = getAuth();
-getRedirectResult(auth)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access Google APIs.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-
-    // The signed-in user info.
-    const user = result.user;
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-});
+import { provider } from '../firebase';
+import { signInWithPopup, signInWithRedirect,  getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const LoginSignupScreen = () => {
+
+    const GoogleLogin = () => {
+        signInWithRedirect(getAuth(), provider)
+        .then((re) => {
+            console.log(re);
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
     return(
         <div className="login-signup-screen">
-            <h1>Login/Signup</h1>
-            <button onClick={signInWithRedirect(auth, provider)}>Signin with Google</button>
+            <h1>Login</h1>
+
+            <div className="logincontainer">
+                <button onClick={ GoogleLogin } class="Login">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="24" fill="currentColor" class = "svglogo">
+                        <path d="M4.376 8.068A5.944 5.944 0 0 0 4.056 10c0 .734.132 1.437.376 2.086a5.946 5.946 0 0 0 8.57 3.045h.001a5.96 5.96 0 0 0 2.564-3.043H10.22V8.132h9.605a10.019 10.019 0 0 1-.044 3.956 9.998 9.998 0 0 1-3.52 5.71A9.958 9.958 0 0 1 10 20 9.998 9.998 0 0 1 1.118 5.401 9.998 9.998 0 0 1 10 0c2.426 0 4.651.864 6.383 2.302l-3.24 2.652a5.948 5.948 0 0 0-8.767 3.114z"></path>
+                    </svg>
+
+                    <span>Sign in with Google</span>
+                </button>
+            </div>
+        
         </div>
     )
 }
